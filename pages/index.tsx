ab687924a7e7237056d9769fe690dfab5121e425
omkarhcode <omkarh.code@gmail.com>
2023-11-03 13:50:50 +0530

    added section1 WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import PageLayout from "@/components/layouts/PageLayout";
 import { Box, Flex, Text } from "@chakra-ui/react";
+import Section1 from "@/components/section1";
 
 export default function Home() {
   return (
     <PageLayout>
       {/* Section 1 Start */}
-      <Box>
-        <Text
-          color="white"
-          fontWeight="900"
-          fontSize={{
-            base: "30px",
-            md: "60px",
-          }}
-          lineHeight={{ base: "38px", md: "72px" }}
-          // mb={"14px"}
-          // my={"96"}
-        >
-          Unlock Your Productivity
-        </Text>
-      </Box>
+
+      <Section1 />
+
       {/* Section 1 End */}
     </PageLayout>
   );
