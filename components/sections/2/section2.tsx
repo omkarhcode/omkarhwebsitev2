6ba0edc9f9f681abc80484006ab899b56a7ed317
omkarhcode <omkarh.code@gmail.com>
2023-11-04 17:30:30 +0530

    section 2 blueprint created

---
+++ components/sections/2/section2.tsx
@@ -0,0 +1,26 @@
+// components/sections/1/section1.tsx
+
+import {
+  AbsoluteCenter,
+  Box,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  SimpleGrid,
+  Spacer,
+  Stack,
+  Text,
+  VStack,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const Section1 = () => {
+  return <></>;
+};
+
+export default Section1;
