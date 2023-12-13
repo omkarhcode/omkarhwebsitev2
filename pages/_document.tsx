6587d07a57f8ec06938f64295b8d72eaf7ddc1aa
omkarhcode <omkarh.code@gmail.com>
2023-12-13 14:54:57 +0530

    updated  Google Analytics and GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,13 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script strategy="lazyOnload" id="google-tag-manager">
-        {`
+      <Head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script strategy="lazyOnload" id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -18,8 +18,7 @@ export default function Document() {
           page_path: window.location.pathname,
           });
       `}
-      </Script>
-      <Head>
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
